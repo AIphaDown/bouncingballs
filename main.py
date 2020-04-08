6ed547d357ada8a0e5bf0a8af8d86fe2d5eea455
AIphaDown <stoda2005@gmail.com>
2020-04-08 13:48:41 -0700

    First commit

--- main.py
+++ main.py
@@ -1,41 +1,41 @@
 import pygame
 import random
 from pygame.locals import *
-
+from ball import *
 
 pygame.init()
 
 screen_info = pygame.display.Info()
-screen_size = (screen_info.current_w, screen_info.current_h)
+# screen_size = (screen_info.current_w, screen_info.current_h)
 
 # set size
 size = (width, height) = (850, 480)
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
-color = (32, 187, 50)
+color = (98, 26, 255)
+ballAmount = random.randint(1,50)
 
 # ball
-ball_image = pygame.image.load("ball.png")
-ball_image = pygame.transform.smoothscale(ball_image, (30, 30))
-ball_rect = ball_image.get_rect()
-ball_rect.center = (width//2, height//2)
-speed = pygame.math.Vector2(0, 5)
-speed.rotate_ip(random.randint(0, 360))
-
-def move_ball():
-    ball_rect.move_ip(speed)
+balls = []
 
 def main():
+    for i in range(ballAmount):
+        x = random.randint(50, width-50)
+        y = random.randint(50, height-50)
+        # balls.append(Ball((x*.5, y*.5)))
+        balls.append(Ball((x, y)))
+        # balls.append(Ball((width, height-50)))
     while True:
         clock.tick(60)
-        move_ball()
         for event in pygame.event.get():
             if event.type == QUIT:
                 sys.exit()
         screen.fill(color)
-        screen.blit(ball_image, ball_rect)
+        for ball in balls:
+            ball.update()
+        for ball in  balls:
+            ball.draw(screen)
         pygame.display.flip()
 
 if __name__ == '__main__':
     main()
-
